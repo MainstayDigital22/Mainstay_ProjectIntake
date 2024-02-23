7314e48008223570395c44a7fc40064400d71205
Christopher Liu <116124688+chreamy@users.noreply.github.com>
2024-02-22 23:13:05 -0600

    Update login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const signupSuccess = location.state?.signupSuccess;
   function login(username, password) {
     axios
-      .post(`http://${HOST}:8080/user/login`, {
+      .post(`${HOST}:8080/user/login`, {
         username: username,
         password: password,
       })
@@ -24,7 +24,7 @@ const Login = () => {
           await localStorage.setItem("user", user);
           const json = await JSON.parse(localStorage.getItem("user"));
           console.log(json);
-          axios.get(`http://${HOST}:8080/user/${res.data.user}`).then((res2)=>{
+          axios.get(`${HOST}:8080/user/${res.data.user}`).then((res2)=>{
             if(res2.data[0].onboard){
               window.location.href = "/";
             }else{
